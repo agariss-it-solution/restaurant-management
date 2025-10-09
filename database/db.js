2f082dfa646bf283068daef209cfdc4d120422bd
agariss-it-solution <agarissitsolution@gmail.com>
2025-10-09 13:02:37 +0530

    Delete database directory

--- database/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const mongodb = () => {
-    mongoose.connect(process.env.MONGO_URL).then(() => {
-        console.log("database is conneted");
-    }).catch((err) => {
-        console.log('err', err)
-    })
-}
-module.exports = mongodb
