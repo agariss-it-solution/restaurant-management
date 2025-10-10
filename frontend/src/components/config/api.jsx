8d33f3a4a4119e63e612fed2571f1913a6b4f94f
Jay <jayr6741@gmail.com>
2025-10-10 11:32:08 +0530

    update domin

--- frontend/src/components/config/api.jsx
+++ frontend/src/components/config/api.jsx
@@ -1,6 +1,6 @@
 // src/config/api.js
 import axios from "axios";
-const API_URL = "http://api.mksfood.in:1020/v1/auth"; // Base URL for auth and tables
+const API_URL = "http://mksfood.in/api/v1/auth"; // Base URL for auth and tables
 
 // 🔹 Helper to get token
 const getToken = () => {
@@ -649,7 +649,7 @@ export const cancelOrderItem = async ({ orderId, itemId }) => {
 
   try {
     const res = await axios.post(
-      "http://api.mksfood.in:1020/v1/auth/orders/cancel",
+      "http://mksfood.in/api/v1/auth/orders/cancel",
       { orderId, itemId },
       { headers: { Authorization: `Bearer ${token}` } }
     );
